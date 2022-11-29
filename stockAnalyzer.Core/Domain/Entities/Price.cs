6c967203efad87b905df7b24237491144450b1fe
Karima El Khayati <kelkhaya@emeal.nttdata.com>
2022-11-29 21:33:41 +0100

    add wpf application

---
+++ stockAnalyzer.Core/Domain/Entities/Price.cs
@@ -0,0 +1,24 @@
+﻿
+namespace stockAnalyzer.Core.Domain.Entities
+{
+    public class Price
+    {
+        public string Identifier { get; set; }
+
+        public DateTime TradeDate { get; set; }
+
+        public decimal? Open { get; set; }
+
+        public decimal? High { get; set; }
+
+        public decimal? Low { get; set; }
+
+        public decimal? Close { get; set; }
+
+        public int Volume { get; set; }
+
+        public decimal Change { get; set; }
+
+        public decimal ChangePercent { get; set; }
+    }
+}
