bd7705034a73b7f9867fa584c9cf6f7a32bff8d3
Karima El Khayati <kelkhaya@emeal.nttdata.com>
2022-11-30 22:18:34 +0100

    practice best practices for async and  await

--- stockAnalyzer.Windowss/MainWindow.xaml.cs
+++ stockAnalyzer.Windowss/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
 ﻿
 using Newtonsoft.Json;
 using stockAnalyzer.Core.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
+using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Navigation;
 
@@ -22,22 +25,63 @@ namespace stockAnalyzer.Windowss
             InitializeComponent();
         }
 
-        private void Search_Click(object sender, RoutedEventArgs e)
+        private async void Search_Click(object sender, RoutedEventArgs e)
         {
             BeforeLoadingStockData();
 
-            var client = new WebClient();
+            await GetStocks();
 
-            var content = client.DownloadString($"{API_URL}/{StockIdentifier.Text}");
-
-            // Simulate that the web call takes a very long time
-            Thread.Sleep(10000);
-
-            var data = JsonConvert.DeserializeObject<IEnumerable<Price>>(content);
-
-            Stocks.ItemsSource = data;
 
             AfterLoadingStockData();
+
+            /* exemple about diffrence between Task.Result and await 
+                Debug.WriteLine($"Task started");
+
+              var x = Go().Result; USING Task.Result or Task.Wait() caused a deadlock  it blocks the thread untill the task is completed
+
+                 var x = await Go()  await des not bolck the current thrad  ;
+
+
+                Debug.WriteLine("$Task completed ");
+
+            **/
+        }
+
+        private async Task GetStocks()
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var respsonse = await client.GetAsync($"{API_URL}/{StockIdentifier.Text}");
+                    var content = await respsonse.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<IEnumerable<Price>>(content);
+                    Stocks.ItemsSource = data;
+                }
+                catch (Exception ex)
+                {
+                    Notes.Text = ex.Message;
+                }
+            }
+
+            /* var client = new WebClient();
+
+           var content = client.DownloadString($"{API_URL}/{StockIdentifier.Text}");
+
+           // Simulate that the web call takes a very long time
+           Thread.Sleep(10000);
+          */
+
+            // var data = JsonConvert.DeserializeObject<IEnumerable<Price>>(content);
+
+            // Stocks.ItemsSource = data;
+        }
+
+        private async Task<HttpResponseMessage> Go()
+        {
+            var client = new HttpClient();
+            var response =   await client.GetAsync($"{API_URL}/{StockIdentifier.Text}");
+            return response;
         }
 
 
